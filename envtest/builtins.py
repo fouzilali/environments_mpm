1bd0d91f3458e08c4146929f63db4ab695ff29b1
fouzilali <47727917+fouzilali@users.noreply.github.com>
2025-10-06 15:03:37 +0100

    finished lecture 1 tasks

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,14 @@
 import numpy as np
+import scipy
 
-
-__all__ = ['rand_array']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+def smooth_image(a, sigma=1):
+    return scipy.gaussian_filter(a, sigma=sigma)
+
+def my_mat_solve(A,b):
+    return A.inv()*b
